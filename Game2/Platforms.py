a60c9df23ac4ada4dd8718275a6dde06fa0a6e80
RyuZacki <theartem000@gmail.com>
2020-05-06 21:14:34 +0300

    camera

---
+++ Game2/Platforms.py
@@ -0,0 +1,12 @@
+
+from pygame.sprite import Sprite
+from pygame.image import load
+
+class Platform(Sprite):
+    def __init__(self, x, y):
+        Sprite.__init__(self)
+        self.image = load('Platform.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
