ba9ddb81511636aad9b772aeff5ab1b0fed8d657
RyuZacki <theartem000@gmail.com>
2020-06-12 13:05:07 +0300

    123123

--- server/server.py
+++ server/server.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
-
+server_start = datetime.now().strftime('%H:%M:%S %d/%m/%Y')
 
 @app.route("/")
 def hello():
-    return "Hello World! 123"
+    return 'Hello, User! <a href="/status">статус</a>'
 
 @app.route("/status")
 def status():
     return {
-        'status' : 'OK'
+        'status' : 'OK',
+        'name' : 'Skillbox Messenger',
+        'server_start_time' : server_start,
+        'server_current_time' : datetime.now().strftime('%H:%M:%S %d/%m/%Y')
     }
 
 app.run()
