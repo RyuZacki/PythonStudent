9f9aa31b954acc43029b92d7bab7441d44c88ba8
RyuZacki <theartem000@gmail.com>
2020-08-28 19:40:23 +0300

    123123

--- Kodik/test5.2.py
+++ Kodik/test5.2.py
@@ -1,18 +1,2 @@
 
-# Else у циклов For и While
 
-for i in range(5):
-    if i == 6:
-        print(i)
-        break
-else:
-    print('The end')
-
-flag = False
-for i in range(5):
-    if i == 4:
-        flag = True
-        break
-
-if flag:
-    print('Six was found')
